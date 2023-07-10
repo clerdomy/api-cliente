26c94da2003cb1b2a2fd835d276e56b65382154b
clerdomy <clerdomyz@gmail.com>
2023-07-10 19:13:49 -0400

    secret

--- setup/settings.py
+++ setup/settings.py
@@ -11,14 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Restante do arquivo settings.py
-# ...
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-1@0kl-mcivh()lvo9kl2n1gas2$lr3*7tc-ssp%da2pwvxvb-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
