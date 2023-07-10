82c6e87cda8273cc12846a46c6efd85714acc7d9
clerdomy <120287784+clerdomy@users.noreply.github.com>
2023-07-10 18:59:49 -0400

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1@0kl-mcivh()lvo9kl2n1gas2$lr3*7tc-ssp%da2pwvxvb-$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
